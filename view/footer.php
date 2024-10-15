803e5b8220186aaa4571f21e12d2f6ca3e1e622e
Jose-01010000 <josepuello112000@gmail.com>
2024-10-15 10:07:27 -0500

    Cambios en footer 🔗

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,40 @@
+<?php
+$fecha = date("Y");
+?>
+
 <footer>
-    <section>
-        <!-- <a href="mailto:josepuello@gmail.com">josepuello@gmail.com</a> -->
-    </section>
-    <p>Bio-Clean Earth © 2021, Todos los derechos reservados</p>
+    <div class="footer">
+        <div>
+            <div class="footer-logo">
+                <h1><i class="fas fa-recycle"></i> Bio-Clean Earth</h1>
+            </div>
+            <div class="footer-redes">
+                <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+        <div class="footer-contacto">
+            <h3>Contáctanos</h3>
+            <p><a href="mailto:bio-ce-support@bcesupport.com">bio-ce-support@bcesupport.com</a></p>
+            <p><a href="tel:+573123456789">+57 312 345 6789</a></p>
+        </div>
+        <div class="footer-suscribir">
+            <h3>Suscribete</h3>
+            <form action="" method="post">
+                <input type="email" name="correo" placeholder="Correo electrónico" required>
+                <input class="btn-suscribir" type="submit" name="btnLogin" value="Suscribir">
+            </form>
+        </div>
+    </div>
+    <!-- Separación -->
+    <div class="footer-copy">
+        <hr>
+        <div class="terminos">
+            <p> <?php echo $fecha; ?> &copy; Bio-Celan Earth</p>
+            <a href="#">Terminos y condiciones</a>
+            <a href="#">Politica de privacidad</a>
+        </div>
+    </div>
 </footer>
